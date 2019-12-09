1532da0c15d969c810ea67ec3999512094c062a2
ddz <ddzyan@163.com>
2019-12-09 17:54:04 +0800

    删除1.1.0代码

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 
 module.exports = app => {
   app.beforeStart(async () => {
-    // 连接 consu 服务
+    // 连接 consul 服务
     await app.initClient();
     app.coreLogger.info('consul init is ready');
     // 注册本地服务到consul
     await app.registerService();
     app.coreLogger.info('consul register is ready');
 
-    await app.runSchedule('find_consul');
+    // await app.runSchedule('find_consul');
   });
 
   app.beforeClose(async () => {
